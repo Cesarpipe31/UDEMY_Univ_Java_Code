7eba50db6dc40926b0c1bf15e0680293a179da17
Cesarpipe31 <cesarpipe31ingenieria@gmail.com>
2023-03-22 15:31:40 -0500

    UDEMY Univ Java

--- Z015_tiposPrimitivosEnteros/src/TiposPrimitivosEnteros.java
+++ Z015_tiposPrimitivosEnteros/src/TiposPrimitivosEnteros.java
@@ -24,7 +24,7 @@ public class TiposPrimitivosEnteros {
         
         System.out.println("---------------------");
         System.out.println("INT   INT   INT");
-        short numeroInt = 10;
+        int numeroInt = 10;
         System.out.println("Min_Value: " + Integer.MIN_VALUE);
         System.out.println("Max_Value: " + Integer.MAX_VALUE);
         System.out.println("Bytes: " + Integer.BYTES);
@@ -33,12 +33,32 @@ public class TiposPrimitivosEnteros {
         
         System.out.println("---------------------");
         System.out.println("LONG   LONG   LONG");
-        short numeroLong = 10;
+        long numeroLong = 10;
         System.out.println("Min_Value: " + Long.MIN_VALUE);
         System.out.println("Max_Value: " + Long.MAX_VALUE);
         System.out.println("Bytes: " + Long.BYTES);
         System.out.println("Size: " + Long.SIZE);
         System.out.println("Type: " + Long.TYPE);
+        
+        System.out.println("---------------------");
+        System.out.println("Float   Float   Float");
+        float numeroFloat = 10.0F;
+        System.out.println("Min_Value: " + Float.MIN_VALUE);
+        System.out.println("Max_Value: " + Float.MAX_VALUE);
+        System.out.println("Bytes: " + Float.BYTES);
+        System.out.println("Size: " + Float.SIZE);
+        System.out.println("Type: " + Float.TYPE);
+        
+        System.out.println("---------------------");
+        System.out.println("Double   Double   Double");
+        double numeroDouble = 10.0D;
+        System.out.println("Min_Value: " + Double.MIN_VALUE);
+        System.out.println("Max_Value: " + Double.MAX_VALUE);
+        System.out.println("Bytes: " + Double.BYTES);
+        System.out.println("Size: " + Double.SIZE);
+        System.out.println("Type: " + Double.TYPE);
+
+
     }
     
 }
---
+++ Z018_inferencia_Var/src/InferenciaVar.java
@@ -0,0 +1,12 @@
+public class InferenciaVar {
+    public static void main(String arg[]){
+        var numeroEntero = 10;
+        System.out.println("NumeroEntero : " + numeroEntero);
+        
+        var numeroDouble = 10.0;
+        System.out.println("numeroDouble: " + numeroDouble);
+        
+        var numeroFloat = 10.0F;
+        System.out.println("numeroFloat: " + numeroFloat);        
+    }    
+}
