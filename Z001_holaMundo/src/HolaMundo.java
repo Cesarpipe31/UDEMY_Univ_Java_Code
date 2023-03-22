3458a791af892e0070dacbc799ff1119e6fae212
Cesarpipe31 <cesarpipe31ingenieria@gmail.com>
2023-03-22 10:26:21 -0500

    UDEMY Univ Java

--- Z001_holaMundo/src/HolaMundo.java
+++ Z001_holaMundo/src/HolaMundo.java
@@ -1,5 +1,6 @@
 public class HolaMundo {
     public static void main(String args []){
-        System.out.println("Hola Mundo");
+        int miVariableEntera = 10;
+        System.out.println(miVariableEntera);
     }    
 }
