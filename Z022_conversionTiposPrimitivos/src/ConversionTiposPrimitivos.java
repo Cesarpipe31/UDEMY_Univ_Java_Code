9fc641039af8f8d6a671e502797ec3dadff54f39
Cesarpipe31 <cesarpipe31ingenieria@gmail.com>
2023-03-23 13:20:56 -0500

    UDEMY Univ Java

--- Z022_conversionTiposPrimitivos/src/ConversionTiposPrimitivos.java
+++ Z022_conversionTiposPrimitivos/src/ConversionTiposPrimitivos.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 public class ConversionTiposPrimitivos {
     public static void main(String args[]){
         var edadTexto = String.valueOf(10);
@@ -5,5 +8,11 @@ public class ConversionTiposPrimitivos {
         
         var caracter = "hola".charAt(3);
         System.out.println(caracter);
+        
+        System.out.println("Proporciona un caracter:");
+        var consola = new Scanner(System.in);
+        caracter = consola.nextLine().charAt(0);
+        System.out.println(caracter);
+                
     }    
 }
