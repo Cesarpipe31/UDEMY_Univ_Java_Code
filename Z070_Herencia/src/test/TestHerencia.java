af136b6ff865d69af3be1b2c82e82b7391e5fcf3
Cesarpipe31 <cesarpipe31ingenieria@gmail.com>
2023-05-11 16:45:32 -0500

    publicando

--- Z070_Herencia/src/test/TestHerencia.java
+++ Z070_Herencia/src/test/TestHerencia.java
@@ -9,8 +9,9 @@ public class TestHerencia {
         //Empleado empleado1 = new Empleado("Juan", 5000.00);
         //System.out.println("empleado1 = " + empleado1);
         
-        Cliente cliente1 = new Cliente(new Date(), true, "Karla", 'F', 28, "Calle del cartucho");
-        Cliente cliente2 = new Cliente(new Date(), false, "Super", 'M', 12, "Calle del cartucho");
+        var fecha = new Date();
+        Cliente cliente1 = new Cliente(fecha, true, "Karla", 'F', 28, "Calle del cartucho");
+        Cliente cliente2 = new Cliente(fecha, false, "Super", 'M', 12, "Calle del cartucho");
         System.out.println("cliente1 = " + cliente1);
         System.out.println("cliente2 = " + cliente2);
         
